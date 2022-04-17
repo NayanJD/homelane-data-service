43433045311ae5b7fc722940eaeda32b3f11b98b
Nayan Das <dastms@gmail.com>
2022-04-17 15:51:47 +0530

    Add allowed hosts

--- data_service/settings.py
+++ data_service/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(v&*&6!gi3*p)t$ov=y-wa%!_h#s5$+m%-!^!5s8a^bjmtz60m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
